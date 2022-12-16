2c26e5085ee3b20cd731b058c5f9dae7f5a172cb
Jessica <andradejsscl@gmail.com>
2022-12-16 09:13:43 -0500

    commit tree e6cdc95467bdfccf6c379d554e42085e1c4f0da0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// function superbowlWin(record) {
+//     const result = record.find(record => record.result === "W" );
+//     return !!result ? result.year : undefined;
+//   }
+
+function superbowlWin(record) {
+    const result = record.find(winFinder);
+    return result ? result.year : undefined;
+}
+
+function winFinder(object){
+    if(object.result === "W"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
